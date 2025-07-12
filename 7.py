7e75b8ca7ec0c38752947d0ec0bff7c503df6dca
matheus vinicius <matheus.mvrf@gmail.com>
2025-07-11 21:54:33 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,24 @@
+#variaveis sao usadas para salvar algo na memoria do computador
+#
+#uso nome_variavel = expressão
+nome= 'Matheus'
+sobrenome= 'vinicius'
+print('nome :',nome)
+print('sobrenome :',sobrenome)
+concatenacao='Matheus'+' - '+'vinicius'
+print (concatenacao)
+
+
+
+
+
+
+
+
+#test
+#nome= 'Matheus'
+#sobrenome= 'vinicius'
+#print('nome :',nome)
+#print('sobrenome :',sobrenome)
+#concatenação=nome +' '+ sobrenome
+#print ('concatenação :' , concatenação)
---
+++ 8.py
@@ -0,0 +1,9 @@
+nome='matheus'
+idade=18
+altura=1.72
+peso= 64
+
+####print('nome:',nome ,'\nidade:', idade) 
+#calculo
+imc = peso / altura ** 2
+print('nome:',nome ,'\nidade:', idade,'\nimc =',imc) 
