78583ca65e5a7cfb7caffb7492c602d7eee8eab1
matheus vinicius <matheus.mvrf@gmail.com>
2025-07-16 16:25:49 -0300

    Add files via upload

---
+++ item1/primeiro copy.py
@@ -0,0 +1,9 @@
+input=2
+
+input2=3
+
+adicaoX=('') #subtracaoX=(X), multiplicacao=(X), divisao=(X)
+
+(type (adicaoX))
+adicao=(input + input2)
+if adicaoX: print(adicao)
---
+++ item1/primeiro.py
@@ -0,0 +1,20 @@
+input=9
+
+input2=5
+
+adicaoX=('')
+subtracaoX=('')
+multiplicacaoX=('')
+divisaoX=('')
+
+adicao=(input + input2)
+if adicaoX :print ('adicao:', adicao)
+
+subtracao=(input - input2)
+if subtracaoX :print('subtracao:', subtracao)
+
+multiplicacao=(input * input2)
+if multiplicacaoX :print('multiplicacao:', multiplicacao)
+
+divisao=(input / input2)
+if divisaoX :print('divisao:', divisao)
